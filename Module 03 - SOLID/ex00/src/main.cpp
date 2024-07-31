427c0ef8a32394c77d4b339e6e7453e223652255
Pedro Gonçalves <61185097+IcQuackson@users.noreply.github.com>
2024-07-31 13:54:02 +0000

    m03-ex00: Added ex00 Single Responsibility Principle

---
+++ Module 03 - SOLID/ex00/src/main.cpp
@@ -0,0 +1,18 @@
+
+#include "car.hpp"
+
+int main()
+{
+    Car car;
+
+    car.start();
+    car.accelerate(50);
+    car.shift_gears_up();
+    car.shift_gears_down();
+    car.reverse();
+    car.turn_wheel(90);
+    car.straighten_wheels();
+    car.apply_force_on_brakes(100);
+    car.apply_emergency_brakes();
+    car.stop();
+}
